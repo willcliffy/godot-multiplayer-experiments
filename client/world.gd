1d66138f42453ce1144819eb0b309375902d8610
willcliffy <will@wolfjawstudios.com>
2023-01-23 04:02:27 -0500

    oh boy

--- client/world.gd
+++ client/world.gd
@@ -5,23 +5,27 @@ onready var camera : Camera = $Player/CameraBase/Camera
 
 var show_path = true
 
-func _unhandled_input(event):
-	if not event is InputEventMouseButton or not event.pressed:
-		return
-	if event.button_index != BUTTON_LEFT:
-		return
+func _input(event):
+	if not event is InputEventMouseButton or not event.pressed: return
+	if event.button_index != BUTTON_LEFT: return
 
 	var from = camera.project_ray_origin(event.position)
 	var to = from + camera.project_ray_normal(event.position) * 500
 	var res = get_world().direct_space_state.intersect_ray(from, to, [$Player])
-	if not res:
-		return
+	if not res: return
 
 	var pos = res.position
 	pos.x = round(res.position.x)
 	pos.z = round(res.position.z)
 	pos.y = round(res.position.y)
 
+	var attacking = false
+	# TODO - I don't like this, but it's really simple
+	if res.collider.has_method("get_id"):
+		attacking = true
+		$Player.set_attacking(res.collider.get_id())
+		$MessageBroker.player_requested_attack(res.collider.get_id()) # TODO - send pos to server
+
 	$Target.set_target(pos)
-	$Player.set_moving(pos)
-	$MessageBroker.on_player_move(pos)
+	if not attacking:
+		$MessageBroker.player_requested_move(pos, attacking)
